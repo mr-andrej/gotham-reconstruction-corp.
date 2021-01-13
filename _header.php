e506562145850c8a1a355370adcc4cbcf0969c4d
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-01-13 20:57:12 +0100

    Initial commit

---
+++ _header.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="header_section_top">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="custom_menu">
+                    <ul>
+                        <li><a href="#">Les Supers</a></li>
+                        <li><a href="#">À propos</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
