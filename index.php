216fa0d260acb67bcbaa7c8e2b72a9ff6dd73be6
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-01-14 23:10:17 +0100

    Index 0.02

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <!-- site metas -->
-    <title>Eflyer</title>
+    <title>Les Supers - Gotham Reconstruction Corporation</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -20,7 +20,7 @@
     <!-- Responsive-->
     <link rel="stylesheet" href="css/responsive.css">
     <!-- fevicon -->
-    <link rel="icon" href="images/fevicon.png" type="image/gif"/>
+    <link rel="icon" href="images/favicon.png" type="image/gif"/>
     <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
     <!-- Tweaks for older IEs-->
@@ -46,7 +46,7 @@
     <?php include "_header.php"; ?>
     <!-- header top section start -->
     <!-- logo section start -->
-    <div class="logo_section">
+    <div class="logo_section" style="padding-top: 50px;">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
@@ -68,41 +68,19 @@
                     <a href="jewellery.html">Jewellery</a>
                 </div>
                 <span class="toggle_icon" onclick="openNav()"><img src="images/toggle-icon.png"></span>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Category
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
                 <div class="main">
                     <!-- Another variation with a button -->
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Search this blog">
                         <div class="input-group-append">
                             <button class="btn btn-secondary" type="button"
-                                    style="background-color: #f26522; border-color:#f26522 ">
+                                    style="background-color: black; border-color: black; ">
                                 <i class="fa fa-search"></i>
                             </button>
                         </div>
                     </div>
                 </div>
                 <div class="header_box">
-                    <div class="lang_box ">
-                        <a href="#" title="Language" class="nav-link" data-toggle="dropdown" aria-expanded="true">
-                            <img src="images/flag-uk.png" alt="flag" class="mr-2 " title="United Kingdom"> English <i
-                                    class="fa fa-angle-down ml-2" aria-hidden="true"></i>
-                        </a>
-                        <div class="dropdown-menu ">
-                            <a href="#" class="dropdown-item">
-                                <img src="images/flag-france.png" class="mr-2" alt="flag">
-                                French
-                            </a>
-                        </div>
-                    </div>
                     <div class="login_menu">
                         <ul>
                             <li><a href="#">
@@ -121,22 +99,19 @@
     </div>
     <!-- header section end -->
 
-
-
-
     <!-- Featured section -->
     <div class="featured-section spad">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
                     <div class="featured-item">
-                        <img src="images/featured/featured-1.jpg" alt="">
+                        <img src="images/marvel_logo.jpg" alt="">
                         <a href="#" class="site-btn">see more</a>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="featured-item mb-0">
-                        <img src="images/featured/featured-2.jpg" alt="">
+                        <img src="images/DC_logo.jpg" alt="">
                         <a href="#" class="site-btn">see more</a>
                     </div>
                 </div>
@@ -145,20 +120,15 @@
     </div>
     <!-- Featured section end -->
 
-
     <!-- Product section -->
-    <section class="product-section spad">
+    <section class="product-section spadTwo">
         <div class="container">
-            <ul class="product-filter controls">
-                <li class="control" data-filter=".new">New arrivals</li>
-                <li class="control" data-filter="all">Recommended</li>
-                <li class="control" data-filter=".best">Best sellers</li>
-            </ul>
             <div class="row" id="product-filter">
                 <div class="mix col-lg-3 col-md-6 best">
                     <div class="product-item">
                         <figure>
                             <img src="images/products/1.jpg" alt="">
+                            <div class="bache">DC</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -181,7 +151,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/2.jpg" alt="">
-                            <div class="bache">NEW</div>
+                            <div class="bache sale">MARVEL</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -204,6 +174,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/3.jpg" alt="">
+                            <div class="bache">DC</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -226,7 +197,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/4.jpg" alt="">
-                            <div class="bache sale">SALE</div>
+                            <div class="bache sale">MARVEL</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -249,6 +220,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/5.jpg" alt="">
+                            <div class="bache">DC</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -271,7 +243,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/6.jpg" alt="">
-                            <div class="bache">NEW</div>
+                            <div class="bache sale">MARVEL</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -294,6 +266,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/7.jpg" alt="">
+                            <div class="bache sale">MARVEL</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -316,6 +289,7 @@
                     <div class="product-item">
                         <figure>
                             <img src="images/products/8.jpg" alt="">
+                            <div class="bache">DC</div>
                             <div class="pi-meta">
                                 <div class="pi-m-left">
                                     <img src="images/icons/eye.png" alt="">
@@ -389,99 +363,9 @@
     <!-- Blog section end -->
 
 
-    <!-- Footer top section -->
-    <section class="footer-top-section home-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-8 col-sm-12">
-                    <div class="footer-widget about-widget">
-                        <img src="images/logo.png" class="footer-logo" alt="">
-                        <p>Donec vitae purus nunc. Morbi faucibus erat sit amet congue mattis. Nullam fringilla faucibus
-                            urna, id dapibus erat iaculis ut. Integer ac sem.</p>
-                        <div class="cards">
-                            <img src="images/cards/5.png" alt="">
-                            <img src="images/cards/4.png" alt="">
-                            <img src="images/cards/3.png" alt="">
-                            <img src="images/cards/2.png" alt="">
-                            <img src="images/cards/1.png" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-6">
-                    <div class="footer-widget">
-                        <h6 class="fw-title">usefull Links</h6>
-                        <ul>
-                            <li><a href="#">Partners</a></li>
-                            <li><a href="#">Bloggers</a></li>
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Press</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-6">
-                    <div class="footer-widget">
-                        <h6 class="fw-title">Sitemap</h6>
-                        <ul>
-                            <li><a href="#">Partners</a></li>
-                            <li><a href="#">Bloggers</a></li>
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                            <li><a href="#">Press</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-6">
-                    <div class="footer-widget">
-                        <h6 class="fw-title">Shipping & returns</h6>
-                        <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Track Orders</a></li>
-                            <li><a href="#">Returns</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Shipping</a></li>
-                            <li><a href="#">Blog</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-4 col-sm-6">
-                    <div class="footer-widget">
-                        <h6 class="fw-title">Contact</h6>
-                        <div class="text-box">
-                            <p>Your Company Ltd </p>
-                            <p>1481 Creekside Lane Avila Beach, CA 93424, </p>
-                            <p>+53 345 7953 32453</p>
-                            <p>office@youremail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Footer top section end -->
-
-    <!-- Footer section -->
-    <footer class="footer-section">
-        <div class="container">
-            <p class="copyright">
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-                All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
-                        href="https://colorlib.com" target="_blank">Colorlib</a>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-        </div>
-    </footer>
-    <!-- Footer section end -->
+    <?php include "_footer.php"; ?>
 
 
-    <!-- footer section start
-    <div class="footer_section layout_padding">
-       <div class="container">
-          <div class="footer_logo"><a href="index.php"><img src="images/footer-logo.png"></a></div>
-       </div>
-    </div>
-     copyright section end -->
     <!-- Javascript files-->
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
